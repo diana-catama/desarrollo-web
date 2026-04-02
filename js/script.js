ca0c7533bf2b7d35668a2e418df240ff9a4b7fb0
caroo <dianacarolinacatama@gmail.com>
2026-04-01 21:27:13 -0500

    cambios

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 document.addEventListener("DOMContentLoaded", () =>{
-    const move = document.querySelector(".carrousel-move");
-    const btnNext = document.querySelector(".btn-next");
-    const btnBack = document.querySelector(".btn-back");
-    const options = document.querySelectorAll(".option")
+    const move = document.querySelector(".carrousel__move");
+    const btnNext = document.querySelector(".carrousel__btn--next");
+    const btnBack = document.querySelector(".carrousel__btn--back");
+    const elements = document.querySelectorAll(".fade");
 
     let index = 0;
     const total = 3;
 
     function updateCarrousel() {
-        move.style.transform = `translateX(-${index * 220}px)`;
-    }
+    const width = document.querySelector(".carrousel__window").offsetWidth;
+    move.style.transform = `translateX(-${index * width}px)`;
+}
 
     btnNext.addEventListener("click", () => {
         index++;
@@ -22,4 +23,11 @@ document.addEventListener("DOMContentLoaded", () =>{
         if (index < 0) index = total - 1;
         updateCarrousel();
     });
-});
+    window.addEventListener("scroll", () => {
+    elements.forEach(el => {
+        const top = el.getBoundingClientRect().top;
+        if (top < window.innerHeight - 100) {
+            el.classList.add("show");
+        }
+    });
+})})
